c56d09796d16d0f875ca16776c4a7a380ebe66f1
Sudin Bagale <sbagale148@gmail.com>
2026-01-15 13:28:12 -0600

    Updated to use modern ethers.js methods
    Deploy.js: Replaced deprecated deployed() with waitForDeployment() and parseEther() methods

--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -6,17 +6,19 @@ async function main() {
   // Deploy Token
   const MyToken = await ethers.getContractFactory("MyToken");
   const token = await MyToken.deploy(1000000);
-  await token.deployed();
-  console.log("MyToken deployed to:", token.address);
+  await token.waitForDeployment();
+  const tokenAddress = await token.getAddress();
+  console.log("MyToken deployed to:", tokenAddress);
   
   // Deploy Token Sale
   const TokenSale = await ethers.getContractFactory("TokenSale");
-  const tokenSale = await TokenSale.deploy(token.address);
-  await tokenSale.deployed();
-  console.log("TokenSale deployed to:", tokenSale.address);
+  const tokenSale = await TokenSale.deploy(tokenAddress);
+  await tokenSale.waitForDeployment();
+  const tokenSaleAddress = await tokenSale.getAddress();
+  console.log("TokenSale deployed to:", tokenSaleAddress);
   
   // Transfer tokens to sale contract
-  await token.transfer(tokenSale.address, ethers.utils.parseEther("500000"));
+  await token.transfer(tokenSaleAddress, ethers.parseEther("500000"));
   console.log("Transferred 500,000 tokens to sale contract");
 }
 
