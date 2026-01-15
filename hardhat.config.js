983c6db4668e7b39e89b9e75bd2db4acb9078227
Sudin Bagale <sbagale148@gmail.com>
2026-01-15 13:28:30 -0600

    Updated to use modern Hardhat toolbox
    hardhat.config.js: Updated to use @nomicfoundation/hardhat-toolbox instead of deprecated packages and improved environment variable handling

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.SEPOLIA_URL || `https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   },
   paths: {
